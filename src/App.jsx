c9822e8c0043050302bb371ecd2773a7c0e6dcc4
Prim5v <emax27348@gmail.com>
2026-03-07 04:20:09 +0300

    extra email updates 2

--- src/App.jsx
+++ src/App.jsx
@@ -70,7 +70,7 @@ if (authStatus === "unauthenticated") {
       <Route path="/privacy-policy" component={PrivacyPolicy} />
       <Route path="/terms-of-service" component={TermsOfService} />
       <Route path="/forgot-password" component={ForgotPassword} />
-      <Route path="/reset-pasword" component={ResetPassword} />
+      <Route path="/reset-password" component={ResetPassword} />
 
       {/* <Route component={Unauthorized}/> */}
 
@@ -197,7 +197,7 @@ if (authStatus === "authenticated") {
       <Route path="/privacy-policy" component={PrivacyPolicy} />
       <Route path="/terms-of-service" component={TermsOfService} />
       <Route path="/forgot-password" component={ForgotPassword} />
-      <Route path="/reset-pasword" component={ResetPassword} />
+      <Route path="/reset-password" component={ResetPassword} />
       <Route path="/landlord-signup">
         <LandlordSignUp />
       </Route>
